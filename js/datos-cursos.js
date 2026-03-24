8be35136039ca18313cc44b6e2d13f5654d43260
yoyi-88 <ygomben2510@g.educaand.es>
2026-03-24 14:51:57 +0100

    Adición infografías a html dinámico

---
+++ js/datos-cursos.js
@@ -0,0 +1,102 @@
+const MIS_CURSOS = {
+    "veriFactu": {
+        titulo: "¿Qué es VeriFactu y cómo te afecta?",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/1_verifactu.webp"
+    },
+    "facturas": {
+        titulo: "Cómo emitir facturas correctamente (y evitar sanciones)",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/2_factura.webp"
+    },
+    "iva": {
+        titulo: "IVA para autónomos sin complicaciones",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/3_iva.webp"
+    },
+    "irpf": {
+        titulo: "IRPF para autónomos: lo básico que debes saber",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/4_irpf.webp"
+    },
+    "control-ingresos": {
+        titulo: "Control de ingresos y gastos (sin ser contable)",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/5_contable.webp"
+    },
+    "alta": {
+        titulo: "Alta de autónomo paso a paso",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/6_alta.webp"
+    },
+    "gastos": {
+        titulo: "Gastos deducibles: qué puedes y qué no puedes deducir",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/7_gastos.webp"
+    },
+    "facturacion": {
+        titulo: "Facturación electrónica y nuevas obligaciones",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/8_facturacion.webp"
+    },
+    "contratar": {
+        titulo: "Contratar a tu primer trabajador",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/9_contratar.webp"
+    },
+    "nominas": {
+        titulo: "Nóminas para no expertos",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/10_nominas.webp"
+    },
+    "calendario-fiscal": {
+        titulo: "Calendario fiscal explicado fácil",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/11_calendario-fiscal.webp"
+    },
+    "hacienda": {
+        titulo: "Inspecciones de Hacienda: cómo actuar",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/12_inspeccion.webp"
+    },
+    "impuestos": {
+        titulo: "Cómo pagar menos impuestos legalmente",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/13_planificacion.webp"
+    },
+    "digitalizacion": {
+        titulo: "Digitalización del autónomo",
+        descripcion: "",
+        video1: "",
+        video2: "",
+        infografia: "images/cursos/infografias/14_digitalizacion.webp"
+    },
+    
+    
+};
