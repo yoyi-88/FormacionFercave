d1ef8814419d34fac52c59598f697d284a03ca05
jrolo92 <colego@hotmail.es>
2026-03-25 08:39:59 +0100

    25/03/26

---
+++ js/firebase-config.js
@@ -0,0 +1,19 @@
+// Importamos solo lo que necesitamos de los servidores de Google
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnh5Q--KLV8r-th5KTGcJM7GpPmv4lbys",
+    authDomain: "formacion-fercave.firebaseapp.com",
+    projectId: "formacion-fercave",
+    storageBucket: "formacion-fercave.firebasestorage.app",
+    messagingSenderId: "491872232017",
+    appId: "1:491872232017:web:72ea809b6dd4c4849d4e70",
+    measurementId: "G-58WGQZK8PX"
+};
+
+// Arrancamos Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportamos la "llave" de autenticación para usarla en otros archivos
+export const auth = getAuth(app);
---
+++ js/login.js
@@ -0,0 +1,22 @@
+import { auth } from './firebase-config.js';
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+const form = document.querySelector('.form-login');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); // Evitamos que la página se recargue
+
+    const email = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Intentamos entrar
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // ¡Éxito! Nos vamos al index
+            window.location.href = "index.html";
+        })
+        .catch((error) => {
+            // Si hay error (contraseña mal, usuario no existe...)
+            alert("Error de autenticación");
+        });
+});
